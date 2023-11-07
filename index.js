74a244d47a69f9f4963ec82379d20dbfaa3ab16e
Hayri Cicek <hayri.cicek@hiq.se>
2023-11-07 15:13:59 +0100

    Adds mock data

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from "express";
+import data from "./data/mock.json" assert { type: "json" };
 
 const app = express();
 
 const PORT = 3000;
 
+app.get("/", (request, response) => {
+  response.json(data);
+});
+
+app.post("/create", (request, response) => {
+  response.send("This is a POST request at /create");
+});
+
+app.put("/edit", (request, response) => {
+  response.send("This is a PUT request at /edit");
+});
+
+app.delete("/delete", (request, response) => {
+  response.send("This is a DELETE request at /delete");
+});
+
 app.listen(PORT, () => {
   console.log(`The server is running on ${PORT}!`);
+  console.log(data);
 });
